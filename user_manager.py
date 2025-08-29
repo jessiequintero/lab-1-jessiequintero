c09c4b5d4b8097da7e90a5921bc163a81e0933b7
jessiequintero <113272508+jessiequintero@users.noreply.github.com>
2025-08-29 02:55:56 +0000

    R1 ADD USER

--- user_manager.py
+++ user_manager.py
@@ -27,3 +27,11 @@ class UserManager:
 
 
 if __name__ == "__main__":
+    user_manager = UserManager()
+    
+    for i in range (500):
+        user_manager.add_user(i,f"Yo soy el num: {i}")
+
+    print("end")
+
+
